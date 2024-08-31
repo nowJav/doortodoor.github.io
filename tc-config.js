7baac25064dd1e00b5f05d003399c90d9485e98c
nowJav <nowjav@gmail.com>
2024-08-31 01:11:08 -0600

    inicial

---
+++ tc-config.js
@@ -0,0 +1,34 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://Curso_ID1";
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+    "es-ES": "Curso de aprendizaje electrónico de Captivate"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+    "es-ES": "Course Description."
+};
+
+//
+// Pre-configured LRSes that should receive data, added to what is included
+// in the URL and/or passed to the constructor function.
+//
+// An array of objects where each object may have the following properties:
+//
+//    endpoint: (including trailing slash '/')
+//    auth:
+//    allowFail: (boolean, default true)
+//    version: (string, defaults to high version supported by TinCanJS)
+//
+TC_RECORD_STORES = [
+];
